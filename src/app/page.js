73f02d6ee61c3d1eebc509ff0774bab7c5518cd9
mdsalahuddin96 <salauddincse96@gmail.com>
2026-04-14 00:30:56 +0600

    added a navbar from daisyUi

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 
 
-export default async function Home() {
-  
+export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+      <h1 className="btn btn-primary">Hello</h1>
     </div>
   );
 }
