eec5daeb144ff609e7062be02b83181f6f6da31e
mdsalahuddin96 <salauddincse96@gmail.com>
2026-04-15 17:55:41 +0600

    Timeline page No data message show when no data available

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const TimelinesPage = () => {
   return (
-    <div className="bg-[#f8fafc] px-60 py-20 ">
-      <div className="container mx-auto border border-amber-500">
+    <div className="bg-[#f8fafc] p-8 md:px-60 md:py-20 ">
+      <div className="container mx-auto">
         <h1 className="text-5xl font-bold text-[#1f2937]">Timeline</h1>
         <div>
 
--- src/components/timlinesdata/TimelinesData.jsx
+++ src/components/timlinesdata/TimelinesData.jsx
@@ -1,11 +1,18 @@
 'use client'
 import { InteractionsContext } from '@/context/InteractionsContext';
+import { FaRegFileAlt } from "react-icons/fa";
 import React, { useContext } from 'react';
 import InteractionCard from './InteractionCard';
 
 const TimelinesData = () => {
     const {interactions,setInteractions}=useContext(InteractionsContext)
     
+        if(interactions.length===0){
+            return<div className='border border-[#e9e9e9] rounded-xl bg-white min-h-screen flex flex-col gap-4 justify-center items-center text-center'>
+                <FaRegFileAlt color='#244D3F' size={80} />
+                <p className='text-5xl font-bold text-[#244D3F]'>No Data Available</p>
+            </div>
+        }
     return (
         <div className='space-y-4'>
             {interactions.map((interaction,ind)=>
